f9b2a64914a6ac60e147e9f5d8e2eb3d9567a20c
Feridun Erbas <feridunerbas@gmail.com>
2020-07-15 01:44:19 +0300

    Added section creation logic to viewModel with dummy data

--- CellConfigurator/Examples/UITableView/ViewModel/ContactsViewModel.swift
+++ CellConfigurator/Examples/UITableView/ViewModel/ContactsViewModel.swift
@@ -17,4 +17,34 @@ class ContactsViewModelImplementation: ContactsViewModel {
 	var sections: [TableSection] = []
 	var reloadTable: VoidBlock?
 	
+	init() {
+		self.getContacts()
+	}
+	
+	private func getContacts() {
+		
+		// Simulating the async operation
+		DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+			guard let self = self else { return }
+			self.processData(contacts: self.createDummyContacts())
+		}
+	}
+	
+	private func processData(contacts: [Contact]) {
+		self.sections = [
+			ContactsSection(contacts: contacts)
+		]
+		self.reloadTable?()
+		
+	}
+	
+	private func createDummyContacts() -> [Contact] {
+		[
+			Contact(name: "Agent Smith", phone: "+0 (110) 011 1101"),
+			Contact(name: "Neo", phone: "+1 (001) 100 0010"),
+			Contact(name: "James Bond", phone: "+7 (007) 007 0707")
+			
+		]
+	}
+	
 }
