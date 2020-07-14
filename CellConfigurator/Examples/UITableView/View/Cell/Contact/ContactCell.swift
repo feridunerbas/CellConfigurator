ae62f967dfb164f0796a1b60c0fc20a0a2385404
Feridun Erbas <feridunerbas@gmail.com>
2020-07-15 01:51:12 +0300

    Implemented basic UI for contact cell

--- CellConfigurator/Examples/UITableView/View/Cell/Contact/ContactCell.swift
+++ CellConfigurator/Examples/UITableView/View/Cell/Contact/ContactCell.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ContactCell: UITableViewCell {
+	
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var phoneLabel: UILabel!
+	
+	private var viewModel: ContactCellViewModel?
 
     override func awakeFromNib() {
         super.awakeFromNib()
         
     }
 
+	override func configure(with: Any?) {
+		self.viewModel = with as? ContactCellViewModel
+	}
 	
+	override static func estimateHeight(for bounds: CGRect, cellViewModel: Any?) -> CGFloat {
+		return 40
+	}
+	
+	override static func calculateHeight(for bounds: CGRect, cellViewModel: Any?) -> CGFloat {
+		return UITableView.automaticDimension
+	}
     
 }
