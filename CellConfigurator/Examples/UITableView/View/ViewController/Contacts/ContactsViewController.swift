9c507ccc352bfce03f94c568987b77aee05635fe
Feridun Erbas <feridunerbas@gmail.com>
2020-07-15 01:24:57 +0300

    Implemented contacts  table delegate methods

--- CellConfigurator/Examples/UITableView/View/ViewController/Contacts/ContactsViewController.swift
+++ CellConfigurator/Examples/UITableView/View/ViewController/Contacts/ContactsViewController.swift
@@ -30,6 +30,16 @@ class ContactsViewController: UIViewController {
 
 extension ContactsViewController: UITableViewDelegate {
 	
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		let cellViewModel = viewModel.cellViewModel(at: indexPath)
+		return cellConfigurator.lookUp(cellViewModel: cellViewModel).calculateHeight(for: tableView.bounds, cellViewModel: cellViewModel)
+	}
+	
+	func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+		let cellViewModel = viewModel.cellViewModel(at: indexPath)
+		return cellConfigurator.lookUp(cellViewModel: cellViewModel).estimateHeight(for: tableView.bounds, cellViewModel: cellViewModel)
+		
+	}
 }
 
 extension ContactsViewController: UITableViewDataSource {
